c77fd57d906fedd7d7ddb5f28b6b16fb5b2b0e71
DILSHODBEK <dilshodsattar26@gmail.com>
2026-06-01 02:24:37 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class filterFrocessWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = filterFrocessWorker()
+    print("Worker engine initialized.")
